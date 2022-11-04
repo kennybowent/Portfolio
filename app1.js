be79297744093c014abfe430d41b845f63b71e32
kennybowent <kennybowent@gmail.com>
2022-11-03 17:13:37 -0700

    home page elements all done

---
+++ app1.js
@@ -0,0 +1,17 @@
+// const homeButton = document.getElementById("#homebutton")
+
+
+// function hideIcon () {
+//     document.getElementById('homeicon').style.visibility = "hidden";
+// }
+
+// homeButton.addEventListener("mouseover", hideIcon)
+
+// const icon = document.getElementById("#homeicon")
+// const button = document.getElementById("#homebutton")
+
+// function hover() {
+//     document.getElementById("#homeicon").style.display = "none"
+// }
+
+// button.addEventListener("click", hover)
